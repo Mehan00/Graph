5f1d5fb6ff7e64946cfcd134f59eba865e0e61aa
Mehan00 <73178715+Mehan00@users.noreply.github.com>
2021-05-22 21:58:54 +0200

    Update ControlPanel.java

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -3,10 +3,10 @@ import javax.swing.JPanel;
 
 //klasa do panelu sterowania
 //tutaj będą podawane współrzędne na podstawie, których w klasie GraphPanel będą rysowane
-class ControlPanel<DrawMainFrame> extends JPanel{
-    private DrawMainFrame parentFrame;
+class ControlPanel extends JPanel{
+    private GraphDraw parentFrame;
 	
-		public ControlPanel(DrawMainFrame parentFrame) {
+		public ControlPanel(GraphDraw parentFrame) {
 			this.parentFrame=parentFrame;
 			setPreferredSize(new Dimension(600,200));
 		
